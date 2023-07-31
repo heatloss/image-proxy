2b955483fb5d3c2dd0b7906825d24afa74da2a39
Glitch (image-url-proxy) <none>
2023-07-31 13:53:20 +0000

    Hybrid proxy, via Glitch

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import http from 'http'
 import fetch from 'node-fetch'
 import sharp from 'sharp'
 import apicache from 'apicache'
+import cors from 'cors'
 
 let cache = apicache.middleware
 
@@ -11,17 +12,22 @@ let app = express()
 let server = http.createServer(app)
 const port = process.env.PORT || 3000
 
+const corsOptions = {
+  origin: ''
+}
 
 app.get('/', (req, res)=>{
   const message = `
-    Visit /proxy?url=<your image url here>&x=<pixel dimensions for x>&y=<pixel dimensions for y> to proxy.
-    For example: 
+    Visit /image/proxy?url=<your image url here>&x=<pixel dimensions for x>&y=<pixel dimensions for y> to proxy.
+    Visit /url/proxy?url=<your url here> to add CORS headers
+    
+    For example: https://image-url-proxy.glitch.me/image/proxy?url=https://www.endcomic.com/wp-content/uploads/2023/06/31.png&x=600
   
   `
   res.end(message)
 })
 
-app.get('/proxy', cache('60 minutes'), async (req, res)=>{
+app.get('/image/proxy', cache('60 minutes'), async (req, res)=>{
   const url = req.query.url
   let original = await fetch(url)
   let original_buffer = Buffer.from(await original.arrayBuffer())
@@ -37,6 +43,15 @@ app.get('/proxy', cache('60 minutes'), async (req, res)=>{
       })
       .webp({ smartSubsample: true })
       .toBuffer()
+  } else if (req.query.x) {
+    output = await sharp(original_buffer)
+      .resize({
+        width: parseInt(req.query.x), 
+        height: undefined,
+        withoutEnlargement: true
+      })
+      .webp({ smartSubsample: true })
+      .toBuffer()
   } else {
     output = await sharp(original_buffer)
       .webp({ smartSubsample: true, effort: 0 })
@@ -44,6 +59,17 @@ app.get('/proxy', cache('60 minutes'), async (req, res)=>{
   }
   res.setHeader('Content-Type', 'image/webp')
   return res.end(output)
+})
+
+app.get('/url', (req, res)=>{
+  const message = "Visit /url/proxy?url=<your url here> to proxy."
+  res.end(message)
+})
+
+app.get('/url/proxy', cors(), async (req, res)=>{
+  console.log(req.query.url)
+  const data = await fetch(req.query.url).then(r=>r.text())
+  res.end(data);
 })
 
 app.get('/demo', async (req, res)=>{
